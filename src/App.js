6c794db2725941e62c65efda211e24b753d23527
Fleuron Gnadekpa <vilfleur@gmail.com>
2021-02-07 15:10:18 +0100

    Separate App from markup to its own script file

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!")
+  );
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
