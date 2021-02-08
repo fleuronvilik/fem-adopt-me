b8e51949df9184e99504c6ea9562b78206f092a3
Fleuron Gnadekpa <vilfleur@gmail.com>
2021-02-08 23:14:02 +0100

    Switch from Pure React to JSX in App and Pet

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, name),
-    React.createElement("h3", {}, animal),
-    React.createElement("h3", {}, breed),
-  ]);
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{animal}</h3>
+      <h3>{breed}</h3>
+    </div>
+  );
 }
